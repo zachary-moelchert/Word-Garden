748d4ad7dd24922f901cd241f728784128b13c24
Zachary Moelchert <zachary.moelchert@bc.edu>
2021-02-14 15:28:08 -0500

    Updated through 3.3

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -14,24 +14,45 @@ class ViewController: UIViewController {
     @IBOutlet weak var wordsInGameLabel: UILabel!
     
     @IBOutlet weak var wordBeingRevealedLabel: UILabel!
-    @IBOutlet weak var guestLetterTextField: UITextField!
+    @IBOutlet weak var guessedLetterTextField: UITextField!
     @IBOutlet weak var guessLetterButton: UIButton!
     @IBOutlet weak var playAgainButton: UIButton!
     @IBOutlet weak var gameStatusMessageLabel: UILabel!
     @IBOutlet weak var flowerImageView: UIImageView!
     
     
-    
+    func enabledButton() {
+        let text = guessedLetterTextField.text!
+        guessLetterButton.isEnabled = !(text.isEmpty)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        enabledButton()
     }
-
+    
+    func updateUIAfterGuess() {
+        guessedLetterTextField.resignFirstResponder()
+        guessedLetterTextField.text! = ""
+        guessLetterButton.isEnabled = false
+    }
+        
+    @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+        enabledButton()
+    }
+    
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        updateUIAfterGuess()
+    }
+    
     @IBAction func guessALetterButtonPressed(_ sender: UIButton) {
+        updateUIAfterGuess()
     }
     
     @IBAction func playAgainButtonPressed(_ sender: UIButton) {
     }
+    
 }
+
 
